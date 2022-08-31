2028bb042f50de81e29954c102f789b9fe573af5
MinadukiSekina <taroufujimoto075@gmail.com>
2022-08-31 21:52:46 +0900

    Update Context.

---
+++ VRCToolBox/Migrations/PhotoContextMigration/20220831125115_UpdateContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VRCToolBox.Migrations.PhotoContextMigration
+{
+    public partial class UpdateContext : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
