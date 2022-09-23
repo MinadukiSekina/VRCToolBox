2e49534ebe27e5c347233e23af07c799ed31904f
MinadukiSekina <taroufujimoto075@gmail.com>
2022-09-23 18:05:32 +0900

    Bug fix.

--- VRCToolBox/UnityEntry/UnityEntry.cs
+++ VRCToolBox/UnityEntry/UnityEntry.cs
@@ -13,9 +13,45 @@ namespace VRCToolBox.UnityEntry
 {
     public class UnityEntry : ViewModelBase
     {
-        public string DirectoryName { get; set; } = string.Empty;
-        public string Path { get; set; } = string.Empty;
-        public string UnityEditorVersion { get; set; } = string.Empty;
-        public string SDKVersion { get; set; } = string.Empty;
+        private string _directoryName = string.Empty;
+        public string DirectoryName
+        {
+            get => _directoryName;
+            set
+            {
+                _directoryName = value;
+                RaisePropertyChanged();
+            }
+        }
+            private string _path = string.Empty;
+        public string Path
+        {
+            get => _path;
+            set
+            {
+                _path = value;
+                RaisePropertyChanged();
+            }
+        }
+        private string _unityEditorVersion = string.Empty;
+        public string UnityEditorVersion
+        {
+            get => _unityEditorVersion;
+            set
+            {
+                _unityEditorVersion = value;
+                RaisePropertyChanged();
+            }
+        }
+        private string _sdkVersion = string.Empty;
+        public string SDKVersion
+        {
+            get => _sdkVersion;
+            set
+            {
+                _sdkVersion = value;
+                RaisePropertyChanged();
+            }
+        }
     }
 }
