90f5c55ec6f8aa7308459b3f559b6e5bcd86a44c
MinadukiSekina <taroufujimoto075@gmail.com>
2022-10-12 22:42:43 +0900

    Revert "Revert "Add for reset.""
    This reverts commit 9eabe3aefa4e7367062f7e5590e0e539d26d5264.

---
+++ VRCToolBox/Migrations/PhotoContextMigration/20221012124857_AddUserData.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VRCToolBox.Migrations.PhotoContextMigration
+{
+    public partial class AddUserData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "WorldAuthor",
+                table: "Worlds",
+                newName: "AuthorId");
+
+            migrationBuilder.RenameColumn(
+                name: "AvatarAuthor",
+                table: "Avatars",
+                newName: "AuthorId");
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    UserId = table.Column<string>(type: "TEXT", nullable: false),
+                    VRChatName = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.UserId);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_VRChatName",
+                table: "Users",
+                column: "VRChatName");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.RenameColumn(
+                name: "AuthorId",
+                table: "Worlds",
+                newName: "WorldAuthor");
+
+            migrationBuilder.RenameColumn(
+                name: "AuthorId",
+                table: "Avatars",
+                newName: "AvatarAuthor");
+        }
+    }
+}
