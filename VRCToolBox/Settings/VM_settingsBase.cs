e7e05ec1ff990ee60ea5112c467cb1677adab1ec
MinadukiSekina <taroufujimoto075@gmail.com>
2022-10-24 22:00:54 +0900

    Add new settings control.

---
+++ VRCToolBox/Settings/VM_settingsBase.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VRCToolBox.Common;
+using Reactive.Bindings;
+using Reactive.Bindings.Extensions;
+using ModernWpf.Controls;
+
+namespace VRCToolBox.Settings
+{
+    public class VM_settingsBase : ViewModelBase
+    {
+        public ReactivePropertySlim<ViewModelBase> Content { get; } = new ReactivePropertySlim<ViewModelBase>();
+        public ReactiveCommand<NavigationViewItemBase> ChangeContentCommand { get; } = new ReactiveCommand<NavigationViewItemBase>();
+
+        public VM_settingsBase()
+        {
+            //Content = new ReactivePropertySlim<ViewModelBase>();
+            ChangeContentCommand.Subscribe(n => ChangeContent(n)).AddTo(_compositeDisposable);
+        }
+        private void ChangeContent(NavigationViewItemBase item)
+        {
+            try
+            {
+                if (item is null) return;
+                var vm = Activator.CreateInstance((Type)item.Tag);
+                if (vm is null) return;
+                Content.Value.Dispose();
+                Content.Value = (ViewModelBase)vm;
+            }
+            catch (Exception ex)
+            {
+                // TODO : Do something.
+            }
+        }
+    }
+}
