bd77c35b9968e6cf9a9cd14bf9ec2cd2863b0a19
MinadukiSekina <taroufujimoto075@gmail.com>
2022-10-27 20:47:01 +0900

    Modify settings view, Add function to write Unity list.

---
+++ VRCToolBox/Settings/UnitySettings/M_UnityOperator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace VRCToolBox.Settings.UnitySettings
+{
+    internal class M_UnityOperator
+    {
+        internal static async Task WriteListToVCCAsync()
+        {
+            string destJsonPath = $@"{Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)}\VRChatCreatorCompanion\settings.json";
+            if (!File.Exists(destJsonPath))
+            {
+                throw new FileNotFoundException("VCCの設定ファイルが見つかりませんでした。");
+            }
+            VCCSettings? vCCSettings = await VCCSettings.GetVCCSettingsAsync();
+            if (vCCSettings is null)
+            {
+                throw new InvalidOperationException("VCCの設定ファイルを読み込めませんでした。");
+            }
+            using (var fs = new FileStream(destJsonPath, FileMode.Create, FileAccess.ReadWrite, FileShare.ReadWrite, 4098, true))
+            {
+                vCCSettings.userProjects = UnityEntry.UnityOperator.GetUnityProjects(true).Select(x => x.FullName).ToArray();
+                await JsonSerializer.SerializeAsync(fs, vCCSettings, JsonUtility.Options);
+            }
+        }
+    }
+}
--- VRCToolBox/Settings/UnitySettings/VM_UnitySettings.cs
+++ VRCToolBox/Settings/UnitySettings/VM_UnitySettings.cs
@@ -14,6 +14,7 @@ namespace VRCToolBox.Settings.UnitySettings
         public ReactiveProperty<bool> UseVCCUserProjects { get; } = new ReactiveProperty<bool>();
         public ReactiveProperty<string> ProjectBackupsDirectory { get; } = new ReactiveProperty<string>();
         public ReactiveProperty<bool> UseVCCProjectBackupPath { get; } = new ReactiveProperty<bool>();
+        public AsyncReactiveCommand WriteListToVCCAsyncCommand { get; } = new AsyncReactiveCommand();
         public VM_UnitySettings() : this(new M_Settings()) { }
         public VM_UnitySettings(M_Settings m_Settings) : base(m_Settings)
         {
@@ -21,6 +22,33 @@ namespace VRCToolBox.Settings.UnitySettings
             UseVCCUserProjects      = _settings.UseVCCUserProjects.ToReactivePropertyAsSynchronized(d => d.Value).AddTo(_compositeDisposable);
             ProjectBackupsDirectory = _settings.ProjectBackupsDirectory.ToReactivePropertyAsSynchronized(d => d.Value).AddTo(_compositeDisposable);
             UseVCCProjectBackupPath = _settings.UseVCCProjectBackupPath.ToReactivePropertyAsSynchronized(d => d.Value).AddTo(_compositeDisposable);
+            WriteListToVCCAsyncCommand.Subscribe(_ => WriteListToVCCAsync()).AddTo(_compositeDisposable);
+        }
+        private async Task WriteListToVCCAsync()
+        {
+            try
+            {
+                await M_UnityOperator.WriteListToVCCAsync();
+                var message = new MessageContent()
+                {
+                    Button = MessageButton.OK,
+                    DefaultResult = MessageResult.OK,
+                    Icon = MessageIcon.Information,
+                    Text = $@"VCCの設定ファイルへリストを書き込みました。"
+                };
+                message.ShowMessage();
+            }
+            catch (Exception ex)
+            {
+                var message = new MessageContent()
+                {
+                    Button = MessageButton.OK,
+                    DefaultResult = MessageResult.OK,
+                    Icon = MessageIcon.Exclamation,
+                    Text = $@"申し訳ありません。エラーが発生しました。{Environment.NewLine}{ex.Message}"
+                };
+                message.ShowMessage();
+            }
         }
     }
 }
