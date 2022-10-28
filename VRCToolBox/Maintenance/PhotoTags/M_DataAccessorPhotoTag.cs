d66a368de977881e57a8c7dc1de14ec765ad6ddd
MinadukiSekina <taroufujimoto075@gmail.com>
2022-10-29 07:53:23 +0900

    Modify tag function.

--- VRCToolBox/Maintenance/PhotoTags/M_DataAccessorPhotoTag.cs
+++ VRCToolBox/Maintenance/PhotoTags/M_DataAccessorPhotoTag.cs
@@ -4,10 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using VRCToolBox.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
 
 namespace VRCToolBox.Maintenance.PhotoTags
 {
-    public class M_DataAccessorPhotoTag
+    public class M_DataAccessorPhotoTag : ModelBase
     {
         public M_PhotoTag Tag { get; private set; }
 
@@ -17,22 +19,26 @@ namespace VRCToolBox.Maintenance.PhotoTags
         public M_DataAccessorPhotoTag(M_PhotoTag m_PhotoTag)
         {
             Tag = m_PhotoTag;
+            Tag.AddTo(_compositeDisposable);
+            _ = SearchTags();
         }
-        public void SearchTags()
+        public async Task SearchTags()
         {
             PhotoTags.Clear();
             using var context = new PhotoContext();
-            PhotoTags.AddRange(context.PhotoTags.Select(t => new M_PhotoTag(t)));
+            List<PhotoTag> tags = await context.PhotoTags.AsNoTracking().ToListAsync();
+            PhotoTags.AddRange(tags.Select(t => new M_PhotoTag(t)));
         }
-        public void SearchTags(M_PhotoTag tag)
+        public async Task SearchTags(M_PhotoTag tag)
         {
-            SearchTags(tag.TagId.Value);
+            await SearchTags(tag.TagId.Value);
         }
-        public void SearchTags(Ulid tagId)
+        public async Task SearchTags(Ulid tagId)
         {
             PhotoTags.Clear();
             using var context = new PhotoContext();
-            PhotoTags.AddRange(context.PhotoTags.Where(t => t.TagId == tagId).Select(t => new M_PhotoTag(t)));
+            List<PhotoTag> tags = await context.PhotoTags.Where(t => t.TagId == tagId).ToListAsync();
+            PhotoTags.AddRange(tags.Select(t => new M_PhotoTag(t)));
         }
     }
 }
