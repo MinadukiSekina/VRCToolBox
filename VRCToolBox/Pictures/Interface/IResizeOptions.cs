80184333ea03e00ae66cf8a2826a59ab92fea0f8
MinadukiSekina <taroufujimoto075@gmail.com>
2023-07-28 20:11:00 +0900

    Add class of resize option.

--- VRCToolBox/Pictures/Interface/IResizeOptions.cs
+++ VRCToolBox/Pictures/Interface/IResizeOptions.cs
@@ -8,5 +8,40 @@ namespace VRCToolBox.Pictures.Interface
 {
     internal interface IResizeOptions
     {
+        /// <summary>
+        /// リサイズ時の拡大縮小率
+        /// </summary>
+        internal float ScaleOfResize { get; set; }
+
+        /// <summary>
+        /// リサイズ時の品質を指定します
+        /// </summary>
+        internal ResizeMode ResizeMode { get; set; }
+        
+    }
+    /// <summary>
+    /// リサイズ時の品質（補間のレベル？ など）
+    /// </summary>
+    internal enum ResizeMode
+    {
+        /// <summary>
+        /// 指定なし
+        /// </summary>
+        None,
+
+        /// <summary>
+        /// 低品質。ただし速いかも？
+        /// </summary>
+        Low,
+
+        /// <summary>
+        /// 中程度。そこそこの速さ？
+        /// </summary>
+        Medium,
+
+        /// <summary>
+        /// 高品質。ただし遅い？
+        /// </summary>
+        High,
     }
 }
---
+++ VRCToolBox/Pictures/Model/ResizeOptions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VRCToolBox.Pictures.Interface;
+
+namespace VRCToolBox.Pictures.Model
+{
+    internal class ResizeOptions : IResizeOptions
+    {
+        /// <summary>
+        /// リサイズ時のスケール
+        /// </summary>
+        private float _scaleOfResize = 1f;
+
+        /// <summary>
+        /// リサイズ時の品質
+        /// </summary>
+        private ResizeMode _resizeMode = ResizeMode.None;
+
+        float IResizeOptions.ScaleOfResize { get => _scaleOfResize; set => _scaleOfResize = value; }
+        ResizeMode IResizeOptions.ResizeMode { get => _resizeMode; set => _resizeMode = value; }
+    }
+}
