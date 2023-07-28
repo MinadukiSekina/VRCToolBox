b693822bcbe8630db9cab988f1f07d5fbe06a333
MinadukiSekina <taroufujimoto075@gmail.com>
2023-07-28 20:58:35 +0900

    Add webp options.

--- VRCToolBox/Pictures/Interface/IWebpEncoderOptions.cs
+++ VRCToolBox/Pictures/Interface/IWebpEncoderOptions.cs
@@ -8,5 +8,27 @@ namespace VRCToolBox.Pictures.Interface
 {
     internal interface IWebpEncoderOptions
     {
+        /// <summary>
+        /// 可逆圧縮か非可逆圧縮か
+        /// </summary>
+        internal WebpCompression WebpCompression { get; set; }
+
+        /// <summary>
+        /// 変換時の品質
+        /// </summary>
+        internal float Quality { get; set; }
+    }
+
+    internal enum WebpCompression
+    {
+        /// <summary>
+        /// 非可逆圧縮
+        /// </summary>
+        Lossy,
+
+        /// <summary>
+        /// 可逆圧縮
+        /// </summary>
+        Lossless,
     }
 }
---
+++ VRCToolBox/Pictures/Model/WebpEncoderOptions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VRCToolBox.Pictures.Interface;
+
+namespace VRCToolBox.Pictures.Model
+{
+    internal class WebpEncoderOptions : IWebpEncoderOptions
+    {
+        /// <summary>
+        /// 可逆圧縮 or 非可逆圧縮
+        /// </summary>
+        private WebpCompression _webpCompression;
+
+        /// <summary>
+        /// 変換時の品質
+        /// </summary>
+        private float _quality;
+
+        WebpCompression IWebpEncoderOptions.WebpCompression { get => _webpCompression; set => _webpCompression = value; }
+        float IWebpEncoderOptions.Quality { get => _quality; set => _quality = value; }
+    }
+}
