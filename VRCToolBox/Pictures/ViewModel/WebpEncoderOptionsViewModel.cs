b6e806c0e040d57858b1b6debd028112aa318c7a
MinadukiSekina <taroufujimoto075@gmail.com>
2023-07-29 17:48:45 +0900

    Add webp encoder options view model.

---
+++ VRCToolBox/Pictures/ViewModel/WebpEncoderOptionsViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VRCToolBox.Pictures.Interface;
+
+namespace VRCToolBox.Pictures.ViewModel
+{
+    public class WebpEncoderOptionsViewModel : ViewModelBase, IWebpEncoderOptionsViewModel
+    {
+        private IWebpEncoderOptions _model;
+
+        public ReactiveProperty<WebpCompression> WebpCompression { get; }
+
+        public ReactiveProperty<float> Quality { get; }
+
+        internal WebpEncoderOptionsViewModel(IWebpEncoderOptions webpEncoderOptions)
+        {
+            _model  = webpEncoderOptions;
+            Quality = _model.Quality.ToReactivePropertyAsSynchronized(x => x.Value).AddTo(_compositeDisposable);
+            WebpCompression = _model.WebpCompression.ToReactivePropertyAsSynchronized(x=>x.Value).AddTo(_compositeDisposable);
+        }
+    }
+}
