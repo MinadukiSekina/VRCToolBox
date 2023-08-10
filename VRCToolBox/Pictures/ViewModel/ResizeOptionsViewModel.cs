2f1072a09720e85c45fc79017050e15503092513
MinadukiSekina <taroufujimoto075@gmail.com>
2023-08-10 20:27:39 +0900

    Add validation.

--- VRCToolBox/Pictures/ViewModel/ResizeOptionsViewModel.cs
+++ VRCToolBox/Pictures/ViewModel/ResizeOptionsViewModel.cs
@@ -18,7 +18,9 @@ namespace VRCToolBox.Pictures.ViewModel
         internal ResizeOptionsViewModel(IResizeOptions resizeOptions)
         {
             _model        = resizeOptions;
-            ScaleOfResize = _model.ScaleOfResize.ToReactivePropertyAsSynchronized(x => x.Value, x => (int)(x * 100), x => (float)x / 100f).AddTo(_compositeDisposable);
+            ScaleOfResize = _model.ScaleOfResize.ToReactivePropertyAsSynchronized(x => x.Value, x => (int)(x * 100), x => (float)x / 100f, ignoreValidationErrorValue: true, mode: ReactivePropertyMode.Default | ReactivePropertyMode.IgnoreInitialValidationError).
+                                                 SetValidateNotifyError(x => x <= 0 ? "その倍率は指定できません。" : null).
+                                                 AddTo(_compositeDisposable);
             ResizeMode    = _model.ResizeMode.ToReactivePropertyAsSynchronized(x => x.Value).AddTo(_compositeDisposable);
         }
     }
