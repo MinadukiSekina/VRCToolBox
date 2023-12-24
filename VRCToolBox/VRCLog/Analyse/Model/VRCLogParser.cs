bf67250dacbef205be14627c9798c8c431367073
MinadukiSekina <taroufujimoto075@gmail.com>
2023-12-24 20:04:02 +0900

    Testing for speed improvement

--- VRCToolBox/VRCLog/Analyse/Model/VRCLogParser.cs
+++ VRCToolBox/VRCLog/Analyse/Model/VRCLogParser.cs
@@ -10,15 +10,25 @@ namespace VRCToolBox.VRCLog.Analyse.Model
     /// <summary>VRChatのログを解析するクラス</summary>
     internal class VRCLogParser : ILogParser
     {
+        private static readonly Regex _regex  = new Regex(@"(\d{4}\.\d{2}\.\d{2} \d{2}:\d{2}:\d{2}) Log\s+-\s+\[Behaviour\] (Initialized PlayerAPI|OnPlayerLeft|Entering Room:)\s+(.*)", RegexOptions.Compiled);
+        private static readonly Regex _regex2 = new Regex(@"""([^""]+)""\s+(?:is (local|remote))", RegexOptions.Compiled);
+        private static readonly List<string> _logEntries = new List<string>()
+        {
+            "Initialized PlayerAPI",
+            "OnPlayerLeft",
+            "Entering Room:"
+        };
+
         /// <summary>VRChatのログの行を受け取り、解析結果を返します。</summary>
         /// <param name="logLine">VRChatのログの行</param>
         /// <returns>解析結果</returns>
         internal static IParseLogResult? ParseLogLine(string? logLine)
         {
             if (string.IsNullOrWhiteSpace(logLine)) return null;
-            var regex  = new Regex(@"(\d{4}\.\d{2}\.\d{2} \d{2}:\d{2}:\d{2}) Log\s+-\s+\[Behaviour\] (Initialized PlayerAPI|OnPlayerLeft|Entering Room:)\s+(.*)", RegexOptions.Compiled);
-            var regex2 = new Regex(@"""([^""]+)""\s+(?:is (local|remote))", RegexOptions.Compiled);
-            var match  = regex.Match(logLine);
+            if(_logEntries.TrueForAll(x => !logLine.Contains(x))) return null;
+            //var regex  = new Regex(@"(\d{4}\.\d{2}\.\d{2} \d{2}:\d{2}:\d{2}) Log\s+-\s+\[Behaviour\] (Initialized PlayerAPI|OnPlayerLeft|Entering Room:)\s+(.*)", RegexOptions.Compiled);
+            //var regex2 = new Regex(@"""([^""]+)""\s+(?:is (local|remote))", RegexOptions.Compiled);
+            var match  = _regex.Match(logLine);
 
             // 一致しなければ null
             if (!match.Success || match.Groups.Count < 4) return null;
@@ -48,7 +58,7 @@ namespace VRCToolBox.VRCLog.Analyse.Model
                 // Join
                 case "Initialized PlayerAPI":
                     // プレイヤー名とローカルかどうかを取得
-                    match = regex2.Match(details);
+                    match = _regex2.Match(details);
                     if (!match.Success) return null;
 
                     result.PlayerName = match.Groups[1].Value;
